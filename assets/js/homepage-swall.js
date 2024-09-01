62aaa2a221f30cc79201f1946013c93585eb0dc2
SannDcode <maulanaabdul1354@gmail.com>
2024-09-01 17:01:41 +0700

    lagi dirumah

---
+++ assets/js/homepage-swall.js
@@ -0,0 +1,21 @@
+// sweetalert
+
+document.addEventListener('DOMContentLoaded', function() {
+    Swal.fire({
+      title: "<strong style='font-family: 'Raleway';'>Halooo Selamat Datang di Official Website Kami!</strong>",
+      iconHtml: '<img src="assets/img/gif/hello-cute.gif" width="150" style="border: none;">',
+      html: `
+        Jangan lupa untuk mengunjungi
+        <a href="Sosial-Media.html" autofocus target='_blank'><b>Sosial Media</b></a>,
+        kami iyaaaak!!
+      `,
+      showCloseButton: true,
+      showCancelButton: false,
+      focusConfirm: false,
+      confirmButtonText: `
+        <i class="fa fa-thumbs-up"></i> Siaap!
+      `,
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+    
+  });
