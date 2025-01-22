9b02a7d41cb9a76bb497b998d4f73548cb97d559
sanndcode <maulanaabdul1354@gmail.com>
2025-01-22 11:17:05 +0700

    nambah tailwindCSS hehe

--- assets/js/homepage-swall.js
+++ assets/js/homepage-swall.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     Swal.fire({
       title: "<strong style='font-family: 'Raleway';'>Halooo Selamat Datang di Official Website Kami!</strong>",
-      iconHtml: '<img src="assets/img/gif/hello-cute.gif" width="150" style="border: none;">',
+      iconHtml: '<img src="assets/img/gif/hello-cute.gif" width="150" style="border: none; border-radius: 100%;">',
       html: `
         Jangan lupa untuk mengunjungi
         <a href="Sosial-Media.html" target='_blank'><b>Sosial Media</b></a>,
